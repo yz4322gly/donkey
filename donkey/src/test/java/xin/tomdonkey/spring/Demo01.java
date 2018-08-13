aca51b508ddc771867a569157fc0678d8e29dbcc
yz4322gly <yz4322gly@163.com>
2018-08-13 18:08:46 +0800

    Initial commit

---
+++ donkey/src/test/java/xin/tomdonkey/spring/Demo01.java
@@ -0,0 +1,12 @@
+package xin.tomdonkey.spring;
+
+import org.junit.Test;
+
+public class Demo01
+{
+    @Test
+    public void testSimpleLoad()
+    {
+
+    }
+}
---
+++ src/main/java/xin/tomdonkey/App.java
@@ -0,0 +1,13 @@
+package xin.tomdonkey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
